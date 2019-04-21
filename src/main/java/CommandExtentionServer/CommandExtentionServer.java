7eeea515b9cebe03c3622634e54cb245147ced8b
SHO <shojabon@gmail.com>
2019-04-21 13:10:32 +0900

    added Location Client Command Support

--- src/main/java/CommandExtentionServer/CommandExtentionServer.java
+++ src/main/java/CommandExtentionServer/CommandExtentionServer.java
@@ -2,25 +2,32 @@ package CommandExtentionServer;
 
 import SecureSocketChipV1.SSCV1;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class CommandExtentionServer{
 
-
-    SSCV1 chip ;
     ServerSocket socket;
     boolean connectionOpen = false;
 
     boolean accecptCommand = false;
 
+    SSCV1 localClient;
+
     List<SSCV1> clientList = new ArrayList<>();
 
+    int port;
+
     public CommandExtentionServer(int port){
+        this.port = port;
         createFolderIfNotExist(new File("plugins"));
         try {
             socket = new ServerSocket(port);
@@ -33,18 +40,23 @@ public class CommandExtentionServer{
     }
 
     public void openCommandConnection(){
+        if(!connectionOpen) return;
+        if(accecptCommand) return;
         accecptCommand = true;
-        Scanner scanner = new Scanner(System.in);
-        while(accecptCommand){
-
-            String input = scanner.nextLine();
-
-            System.out.println(input);
+        try {
+            localClient = new SSCV1(new Socket("127.0.0.1", port));
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            while(accecptCommand){
+                String input = br.readLine();
+                localClient.getCom().sendMessage(input);
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        scanner.close();
     }
 
-    public void closeCommand(){
+    public void closeCommandConnection(){
         accecptCommand = false;
     }
 
